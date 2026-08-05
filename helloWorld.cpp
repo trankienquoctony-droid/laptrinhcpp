6d8837abceebaa73259fe29e29b248501cf00ed9
Trankienquoc <trankienquoc.tony@gmail.com>
2026-08-05 22:00:56 +0700

    hahahahhahahahahahahahhaha

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string word;
+    cout <<"Nhap ten VO TUONG LAI : "; 
+    cin >> word; 
+    cout << " chao vo tuong lai: " << word << endl; 
+    return 0 ;
+}
